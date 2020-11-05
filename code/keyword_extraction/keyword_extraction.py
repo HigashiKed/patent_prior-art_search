5026d4f08e719127c5a1bf56e59ef0472e4d881a
HigashiKed <higashi@higashinoiMac.local>
2020-11-05 10:56:45 +0900

    keywordextraction少し変更

--- code/keyword_extraction/keyword_extraction.py
+++ code/keyword_extraction/keyword_extraction.py
@@ -2,3 +2,25 @@
 input: text:文章 para:キーワード抽出手法 TFIDFかMultipartiteを想定, keynum:上位keynum件のキーワードをreturn
 output: 上位keynum件のキーワード
 '''
+
+from elasticsearch import Elasticsearch
+
+def get_keyword (text,para,keynum):
+    split_stem(text)
+    return ["a","b","c","d"]
+
+
+def split_stem(sentence):
+    # inputされた文を語幹に分割してSTEMフォルダにpickleで保存
+    es = Elasticsearch("http://localhost:9200")
+    '''
+    # elasticsearch analyzerのmax_token_sizeは10000なので,スペース9000区切りで分割する
+    stem_list = []
+    _body = {"analyzer": "english", "text": sentence}
+    query = es.indices.analyze(body=_body)
+    for token_info in query['tokens']:
+        tmp = (re.sub(r'[0-9]+', "0", token_info['token']))   # 数値は0に置換して語幹をsterm_listへ
+        stem_list.append(tmp)
+
+    return stem_list
+    '''
--- code/main.py
+++ code/main.py
@@ -1,7 +1,15 @@
 from query_input.query_input import get_querydata
+from keyword_extraction.keyword_extraction import get_keyword
 
 if __name__ == '__main__':
-    querydocument_list, docid_list, title_list = get_querydata('clef')
-    print(querydocument_list[0])
-    print(docid_list[0])
-    print(title_list[0])
+    # clefもしくはntcirのクエリを整形
+    param = 'clef'
+    querydocument_list, docid_list, title_list = get_querydata(param)
+
+    #1クエリ毎に上位keynum件のkeywordを得る
+    para = 'TFIDF'  #TFIDFかMultipartiteRankの想定
+    keynum = 100    #検索に使用する上位keynum件のキーワード
+    for i, docid in enumerate(docid_list):
+        keywords = get_keyword(querydocument_list[i],para,keynum)
+        print(keywords)
+        exit()
