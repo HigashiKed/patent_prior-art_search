a4f424529527205e901e8a83ce283dc37cc1c515
HigashiKed <higashi@higashinoiMac.local>
2020-12-01 13:26:46 +0900

    修正

--- code/ela_search/ela_search.py
+++ code/ela_search/ela_search.py
@@ -6,11 +6,14 @@ from elasticsearch import Elasticsearch
 import json
 import ast
 
-def priorartsearch (tmp_keywords,docid,index_name):
+def priorartsearch_1 (tmp_keywords,docid,index_name,prel_file ):
+    """
+    全部orで繋いだクエリで検索
+    """
 
     keywords_query = ' '.join(tmp_keywords)
     es = Elasticsearch("http://localhost:9200")
-    write_file = '../result/TFiDF_AND_4.prel'
+    write_file = prel_file+"_1.prel"
     search_list = []
     if index_name=="clef_patent":
         _body = {
@@ -31,15 +34,16 @@ def priorartsearch (tmp_keywords,docid,index_name):
         _body = '{"query": {"match": {"text": "'+ keywords_query + '"}},"_source" : ["ucid","text"]}'
         """
         
+        
         # 2つのみAND
         
         body_first = '{"query": {"bool" : {"should" : ['
         body_second = ""
         body_second +='{ "match": { "text": { "query": "'
-        for keyword in tmp_keywords[:5]:
+        for keyword in tmp_keywords[:2]:
             body_second += keyword+ " "
         body_second += '" ,"operator": "and"}}},'
-        for keyword in tmp_keywords[5:]:
+        for keyword in tmp_keywords[2:]:
             body_second +='{ "match": { "text": "'+ keyword +'" }},'
         body_third = ']}}}'
         _body = body_first+body_second+body_third
@@ -83,4 +87,78 @@ def priorartsearch (tmp_keywords,docid,index_name):
                 + str(search[1])\
                 + ' STANDARD\n'
             f.write(text)
+    return ave_score
+
+def priorartsearch_2 (tmp_keywords,docid,index_name,prel_file ,first_ave_score):
+
+    es = Elasticsearch("http://localhost:9200")
+    write_file = prel_file+"_2.prel"
+    # 2つのみAND
+    for main_keyword in tmp_keywords:
+        print(main_keyword)
+        """
+        main_keywordを固定して検索する
+        """
+        not_main_keyword = [key for key in tmp_keywords if not key==main_keyword]
+        for tmp_keyword in not_main_keyword:
+            search_list = []
+            body_first = '{"query": {"bool" : {"should" : ['
+            body_second = ""
+            body_second +='{ "match": { "text": { "query": "'
+            body_second += main_keyword+ " "
+            body_second += tmp_keyword
+            body_second += '" ,"operator": "and"}}},'
+            for keyword in [key for key in not_main_keyword if not key==tmp_keyword]:
+                body_second +='{ "match": { "text": "'+ keyword +'" }},'
+            body_third = ']}}}'
+            _body = body_first+body_second+body_third
+            _body = ast.literal_eval(_body)
+            #print(json.dumps(_body,indent=2))
+            query = es.search(index=index_name, body=_body, size = 100,request_timeout=150)
+            sum_score = 0
+            for i in range(len(query['hits']['hits'])):
+                search_list.append([query['hits']['hits'][i]['_source']['ucid'],query['hits']['hits'][i]['_score']])
+                sum_score += float(query['hits']['hits'][i]['_score'])
+                #print(json.dumps(query['hits']['hits'][i],indent=2))
+            #print(search_list)
+            ave_score = sum_score/len(search_list)
+            print(ave_score)
+            if first_ave_score < ave_score:
+                with open(write_file, mode='a') as f:
+                    for j,search in enumerate(search_list):
+                        text = str(docid)\
+                            + ' Q0 '\
+                            + str(search[0].replace( '-' , '' ))\
+                            + ' '\
+                            + str(j + 1)\
+                            + ' '\
+                            + str(search[1])\
+                            + ' STANDARD\n'
+                        f.write(text)
+                break
+        else:
+            continue
+        break
+    else:
+        #一番最初が一番よかったもの
+        search_list = []
+        keywords_query = ' '.join(tmp_keywords)
+        _body = '{"query": {"match": {"text": "'+ keywords_query + '"}},"_source" : ["ucid","text"]}'
+        query = es.search(index=index_name, body=_body, size = 100,request_timeout=150)
+        sum_score = 0
+        for i in range(len(query['hits']['hits'])):
+            search_list.append([query['hits']['hits'][i]['_source']['ucid'],query['hits']['hits'][i]['_score']])
+            sum_score += float(query['hits']['hits'][i]['_score'])
+        ave_score = sum_score/len(search_list)
+        with open(write_file, mode='a') as f:
+            for j,search in enumerate(search_list):
+                text = str(docid)\
+                    + ' Q0 '\
+                    + str(search[0].replace( '-' , '' ))\
+                    + ' '\
+                    + str(j + 1)\
+                    + ' '\
+                    + str(search[1])\
+                    + ' STANDARD\n'
+                f.write(text)
     return ave_score
--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 from query_input.query_input import get_querydata
 from keyword_extraction.keyword_extraction import get_keyword
-from ela_search.ela_search import priorartsearch
+from ela_search.ela_search import priorartsearch_1
+from ela_search.ela_search import priorartsearch_2
 import time
 from tqdm import tqdm
 import json
@@ -10,7 +11,8 @@ if __name__ == '__main__':
     start = time.time()
     param = 'clef'
     querydocument_list, docid_list, title_list = get_querydata(param)
-    keywordlist_memo = "keyword_list_AND_4.txt"
+    keywordlist_memo = "keyword_list_AND_8.txt"
+    prel_file = '../result/TFiDF_AND_8'
     #index_name = "clef_patent"  # description,abstract,claimsに分かれている
     index_name = "clef_text"  # 全てが一つのテキストになっている
 
@@ -28,8 +30,15 @@ if __name__ == '__main__':
         with open(keywordlist_memo, mode='a') as f:
             f.write(docid+"\n"+str(tmp_keywords))
             f.write("\n")
-        first_ave_score = priorartsearch(tmp_keywords,docid,index_name)
+        first_ave_score = priorartsearch_1(tmp_keywords,docid,index_name,prel_file )
         print(first_ave_score)
+        second_ave_score = priorartsearch_2(tmp_keywords,docid,index_name,prel_file,first_ave_score)
+        print(second_ave_score)
+        if first_ave_score<second_ave_score:
+            print(docid)
+            with open("memo_8.txt" ,mode='a') as fi:
+                fi.write(docid+"\n")
+
  
     process_time = time.time() - start
     print("実行時間")
